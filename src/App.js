21c3e0ac23681aad8c057423cd538ba88be0b703
ashish-singh96 <ashishbhadauriya5344@gmail.com>
2023-07-18 18:23:35 +0530

    Successfully Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Page1 from './component/Page1';
+import Page2 from './component/Page2';
+import Page3 from './component/Page3';
+import Page4 from './component/Page4';
+import Page5 from './component/Page5';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Page1/>
+      <Page2/>
+      <Page3/>
+      <Page4/>
+      <Page5/>
     </div>
   );
 }
---
+++ src/component/Page2.js
@@ -0,0 +1,69 @@
+import React from 'react'
+
+const Page2 = () => {
+    return (
+        <div>
+            <section className="text-gray-600 body-font" />
+            <div className="container px-5 py-24 mx-auto ">
+                <div className="flex flex-col text-center w-full mb-20">
+                    <h1 className="text-2xl font-medium title-font mb-4 text-gray-900">Our Facilities</h1>
+
+                </div>
+                <div className="flex flex-wrap -m-4 ">
+                    <div className="p-4 lg:w-1/4 md:w-1/2">
+                        <div className="h-full flex flex-col items-center text-center">
+                            <img alt="team" className="flex-shrink-0 rounded-lg w-full h-auto max-w-full object-cover object-center mb-4" src="https://thumbs.dreamstime.com/b/maple-leaf-21079841.jpg" />
+                        </div>
+                    </div>
+                    <div className="p-4 lg:w-1/4 md:w-1/2">
+                        <div className="h-full flex flex-col items-center text-center">
+                            <img alt="team" className="flex-shrink-0 rounded-lg w-full h-auto max-w-full object-cover object-center mb-4" src="https://thumbs.dreamstime.com/b/maple-leaf-21079841.jpg" />
+
+                        </div>
+                    </div>
+                    <div className="p-4 lg:w-1/4 md:w-1/2">
+                        <div className="h-full flex flex-col items-center text-center">
+                            <img alt="team" className="flex-shrink-0 rounded-lg w-full h-auto max-w-full object-cover object-center mb-4" src="https://thumbs.dreamstime.com/b/maple-leaf-21079841.jpg" />
+
+                        </div>
+                    </div>
+                    <div className="p-4 lg:w-1/4 md:w-1/2">
+                        <div className="h-full flex flex-col items-center text-center">
+                            <img alt="team" className="flex-shrink-0 rounded-lg w-full h-auto max-w-full object-cover object-center mb-4" src="https://thumbs.dreamstime.com/b/maple-leaf-21079841.jpg" />
+                        </div>
+                    </div>
+
+                    <div className="p-4 lg:w-1/4 md:w-1/2">
+                        <div className="h-full flex flex-col items-center text-center">
+                            <img alt="team" className="flex-shrink-0 rounded-lg w-full h-auto max-w-full object-cover object-center mb-4 "  src="https://thumbs.dreamstime.com/b/maple-leaf-21079841.jpg" />
+                        </div>
+                    </div>
+
+                    <div className="p-4 lg:w-1/4 md:w-1/2">
+                        <div className="h-full flex flex-col items-center text-center">
+                            <img alt="team" className="flex-shrink-0 rounded-lg w-full h-auto max-w-full object-cover object-center mb-4" src="https://thumbs.dreamstime.com/b/maple-leaf-21079841.jpg" />
+                        </div>
+                    </div>
+
+                    <div className="p-4 lg:w-1/4 md:w-1/2">
+                        <div className="h-full flex flex-col items-center text-center">
+                            <img alt="team" className="flex-shrink-0 rounded-lg w-full h-auto max-w-full object-cover object-center mb-4" src="https://thumbs.dreamstime.com/b/maple-leaf-21079841.jpg" />
+                        </div>
+                    </div>
+
+                    <div className="p-4 lg:w-1/4 md:w-1/2">
+                        <div className="h-full flex flex-col items-center text-center">
+                            <img alt="team" className="flex-shrink-0 rounded-lg w-full h-auto max-w-full object-cover object-center mb-4" src="https://thumbs.dreamstime.com/b/maple-leaf-21079841.jpg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <section />
+
+
+
+        </div>
+    )
+}
+
+export default Page2
