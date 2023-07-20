aca5c3eca196b8f469b5c8323ee68ba9e1ca4f43
ashish-singh96 <ashishbhadauriya5344@gmail.com>
2023-07-21 00:12:13 +0530

    Some Changes

--- src/component/ImageSlider.js
+++ src/component/ImageSlider.js
@@ -15,8 +15,22 @@ const ImageSlider = ({ images }) => {
     return (
         <div>
             <Slider {...settings} style={{height:"100%",width:"100%"}}>
+            
                 {images.map((image, index) => (
+                    
                     <div key={index}>
+                    <div className="absolute  p-96  px-6 ml-6">
+
+                    <h1 className="text-Black font-bold text-6xl p-0" tyle={{ fontSize: "30px" }}>ITM Group Of Institution:<br/>Transforming Ayurvedic<br/> Education
+                    
+                   </h1>
+                    <p className="">
+                        Welcome to the official website of ITM Group Of Institution, Maharajganj, showcasing our <br></br>
+                        esteemed Ayurvedic college, known for its comprehensive BAMS program integrating traditional <br></br>
+                        healing and modern medical advancements, with a commitment to excellence in education and <br></br>
+                        holistic healing.
+                    </p>
+                </div>
                         <img src={image} alt={`Slide ${index + 1}`}/>
                     </div>
                 ))}
